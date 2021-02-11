976b2dc2bf7f7016315fd0117cb5746fdd30b4a9
Nachoxt17 <ignacioceaglio@gmail.com>
2021-02-11 15:13:24 -0300

    8th Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
-import './App.css'
+import React, { Component } from "react";
+import Web3 from "web3";
+import Navbar from "./Navbar";
+import "./App.css";
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '0x0'
-    }
+      account: "0x0",
+    };
   }
 
   render() {
@@ -17,17 +42,19 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+            <main
+              role="main"
+              className="col-lg-12 ml-auto mr-auto"
+              style={{ maxWidth: "600px" }}
+            >
               <div className="content mr-auto ml-auto">
                 <a
                   href="http://www.dappuniversity.com/bootcamp"
                   target="_blank"
                   rel="noopener noreferrer"
-                >
-                </a>
+                ></a>
 
                 <h1>Hello, World!</h1>
-
               </div>
             </main>
           </div>
